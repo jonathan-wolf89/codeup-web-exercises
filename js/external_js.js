def6fd98b2f5deddb8372c67b4d991fdc0614ce8
jonathan-wolf89 <jonathan.wolf89@gmail.com>
2021-04-15 09:07:34 -0500

    :)

---
+++ js/external_js.js
@@ -0,0 +1,15 @@
+alert("Welcome to my Website!");
+
+var userInput = prompt("What is your favorite color");
+
+if(userInput){
+    alert("Your favorite color is: " + userInput);
+}
+
+if(userInput === "blue") {
+    alert("Awesome!")
+}
+
+else {
+    alert("You chose: " + userInput);
+}
